c2a4f43d9757d6f2a3a30e84ab5a702e4a0ffa56
Lewis Kabui <lewis.maina@andela.com>
2018-08-07 17:45:52 +0300

    (feature) Create SignIn action
    - Create signin action within SignIn component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SignUp extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      password2: ''
+    }
+
+    this.createUser = this.createUser.bind(this);
+  }
+
+  createUser() {
+    const url = 'http://localhost:8000/api';
+
+    const formdata = new FormData();
+    formdata.append('username', document.getElementById('username').value);
+    formdata.append('password', document.getElementById('password').value);
+
+    axios.post(`${url}/users/`, formdata);
+
+  }
 
   render() {
 
@@ -32,7 +56,7 @@ class SignUp extends Component {
           </div>
           <div className="row justify-content-center">
             <div className="form-group col-4">
-              <button type="submit" className="btn btn-primary">Create Account</button>
+              <button type="submit" className="btn btn-primary" onClick={this.createUser}>Create Account</button>
             </div>
           </div>
         </div>
