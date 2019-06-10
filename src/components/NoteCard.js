62055f38491253327fb8b6ba0f63ba78a4836abd
Lewis Kabui <lewis.maina@andela.com>
2019-06-10 21:55:06 +0300

    Convert NoteCard back to a functional component

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,80 +1,57 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import { YesNoModal } from './inputs/inputs';
+import { YesNoModal, FormModal } from './inputs/inputs';
 
-class NoteCard extends Component {
+let titleShortener = (propTitle) => {
+  let title, titleChars = propTitle.length;
 
-  constructor(props) {
-    super(props);
+  if (titleChars >0 && titleChars < 43) {
+    title = propTitle;
+  } else {
+    title = `${propTitle.slice(0, 38)} ...`;
   }
 
-  render() {
-    const titleChars = this.props.title.length;
-    const noteChars = this.props.note.length;
-    let title = '', note = '';
-    
-    if (titleChars >0 && titleChars < 43) {
-      title = this.props.title;
-    } else {
-      title = `${this.props.title.slice(0, 38)} ...`;
-    }
-    
-    if (noteChars >0 && noteChars < 43) {
-      note = this.props.note;
-    } else {
-      note = `${this.props.note.slice(0, 38)} ...`;
-    }
-    
-    return (
-      <div className="col-lg-4 col-md-6 col-sm-12 note-margin">
-        <div className="card">
-          <div className="card-header">
-            { title }
-          </div>
-          <div className="card-body">
-            <p className="card-text">{ note }</p>
-
-            <a href={`#editNoteModal${this.props.id}`} data-toggle="modal" className="card-link">Edit Note</a>
-
-            <a href={`#modalId${this.props.id}`} data-toggle="modal" className="card-link">Delete Note</a>
-          </div>
-          <YesNoModal id={this.props.id} modalTitle="Delete Note?" modalBody="This operation cannot be undone!" buttonLabel="Delete" onClick={this.props.onClick}/>
-          {/* Modal */}
-          <div className="modal fade" tabIndex="-1" role="dialog" id={`editNoteModal${this.props.id}`}>
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <form noValidate className="needs-validation" onSubmit={this.props.onSubmit.bind(this, this.props.id)} id={`editNoteForm${this.props.id}`}>
-                  <div className="modal-header">
-                    <h5 className="modal-title">Edit Note Details</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <div className="form-group">
-                      <label htmlFor="editNoteTitle">Title</label>
-                      <input type="text" className="form-control" id={`editNoteTitle${this.props.id}`} placeholder="Title" onChange={this.props.titleOnChange} defaultValue={this.props.title} required/>
-                      <div id={`titleFeedbackId${this.props.id}`} className="invalid-feedback">Title is a required field!</div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="editNoteDescription">Description</label>
-                      <input type="text" className="form-control" id={`editNoteDescription${this.props.id}`} placeholder="Description" defaultValue={this.props.note} onChange={this.props.noteOnChange} required/>
-                      <div id={`noteFeedbackId${this.props.id}`} className="invalid-feedback">Description is a required field!</div>
-                    </div>
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" className="btn btn-primary">Save changes</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  return title;
 }
 
-export default NoteCard;
+let noteShortener = (propNote) => {
+  let note, noteChars = propNote.length;
+
+  if (noteChars >0 && noteChars < 43) {
+    note = propNote;
+  } else {
+    note = `${propNote.slice(0, 38)} ...`;
+  }
+
+  return note;
+}
+
+export default function NameCard(props) {
+
+  return (
+    <div className="col-lg-4 col-md-6 col-sm-12 note-margin">
+      <div className="card">
+        <div className="card-header">
+          { titleShortener(props.title) }
+        </div>
+        <div className="card-body">
+          <p className="card-text">{ noteShortener(props.note) }</p>
+
+          <a href={`#editNoteModal${props.id}`} data-toggle="modal" className="card-link">Edit Note</a>
+
+          <a href={`#modalId${props.id}`} data-toggle="modal" className="card-link">Delete Note</a>
+        </div>
+        <YesNoModal id={props.id} modalTitle="Delete Note?" modalBody="This operation cannot be undone!" buttonLabel="Delete" onClick={props.onClick}/>
+        <FormModal
+          id={props.id}
+          onSubmit={props.onSubmit}
+          title={props.title}
+          titleOnChange={props.titleOnChange}
+          note={props.note}
+          noteOnChange={props.noteOnChange}
+          />
+      </div>
+    </div>
+  );
+  
+}
